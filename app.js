99847635104f4b2993604a744bcb9a1a754a04dd
Christopher N <christopher_webdev@outlook.com>
2023-08-20 06:37:32 +0100

    edited the html, css, and js  to make it sleeker and  mobile friendly

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
 const rounds = document.querySelector(".rounds");
-const res = document.querySelector(".res");
+const result = document.querySelector(".result");
 const userScoreElem = document.querySelector(".userScore");
 const computerScoreElem = document.querySelector(".computerScore");
 
@@ -39,37 +39,37 @@ function playRPS() {
     const comp = getComputerChoice();
 
     if (user === 0 && comp === "rock") {
-      res.textContent = "Draw - Rock on Rock";
+      result.textContent = "Draw - Rock on Rock";
     } else if (user === 1 && comp === "paper") {
-      res.textContent = "Draw - Paper on Paper";
+      result.textContent = "Draw - Paper on Paper";
     } else if (user === 2 && comp === "scissors") {
-      res.textContent = "Draw - Scissors on Scissors";
+      result.textContent = "Draw - Scissors on Scissors";
     } else if (user === 0 && comp === "paper") {
-      res.textContent = "You Lose - Paper Defeats Rock";
+      result.textContent = "You Lose - Paper Defeats Rock";
       computerScore++;
       computerScoreElem.textContent = computerScore;
     } else if (user === 0 && comp === "scissors") {
-      res.textContent = "You Win - Rock Smashes Scissors";
+      result.textContent = "You Win - Rock Smashes Scissors";
       userScore++;
       userScoreElem.textContent = userScore;
     } else if (user === 1 && comp === "rock") {
-      res.textContent = "You Win - Paper Defeats Rock";
+      result.textContent = "You Win - Paper Defeats Rock";
       userScore++;
       userScoreElem.textContent = userScore;
     } else if (user === 1 && comp === "scissors") {
-      res.textContent = "You Lose - Scissors Defeats Paper";
+      result.textContent = "You Lose - Scissors Defeats Paper";
       computerScore++;
       computerScoreElem.textContent = computerScore;
     } else if (user === 2 && comp === "rock") {
-      res.textContent = "You Lose - Rock Smashes Scissors";
+      result.textContent = "You Lose - Rock Smashes Scissors";
       computerScore++;
       computerScoreElem.textContent = computerScore;
     } else if (user === 2 && comp === "paper") {
-      res.textContent = "You Win - Scissors Defeats Paper";
+      result.textContent = "You Win - Scissors Defeats Paper";
       userScore++;
       userScoreElem.textContent = userScore;
     } else {
-      res.textContent = "Not a valid input. Please Choose Your Fate.";
+      result.textContent = "Not a valid input. Please Choose Your Fate.";
     }
   }
   if (roundsPlayed === 5) {
@@ -79,11 +79,11 @@ function playRPS() {
 
 function declareWinner() {
   if (userScore > computerScore) {
-    res.textContent = "You are the winner!";
+    result.textContent = "You are the winner!";
   } else if (userScore < computerScore) {
-    res.textContent = "Computer wins!";
+    result.textContent = "Computer wins!";
   } else {
-    res.textContent = "It's a tie!";
+    result.textContent = "It's a tie!";
   }
 
   refreshButton.style.display = "block"; // Show the restart button
@@ -116,6 +116,6 @@ function resetGame() {
   userScoreElem.textContent = userScore;
   computerScoreElem.textContent = computerScore;
   rounds.textContent = "0";
-  res.textContent = "";
+  result.textContent = "";
   refreshButton.style.display = "none"; // Hide the restart button
 }
